417489c7d724d0de83daeb8604893a21dd8d0276
Marcin Kalita <marcin.kalita90@gmail.com>
2020-08-12 19:49:53 +0200

    hydraapp born again

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,19 @@ registerSW();
 /* place your code below */
 
 console.log('HELLO 🚀')
+
+const addButton = document.querySelector ('.add-beer');
+
+const closeButton = document.querySelector ('.enclose-beer');
+
+const beerValue = document.querySelector ('.beer__number');
+
+const key = new Date ().toISOString ().slice(0,10);
+
+const myNumber = beerValue;
+
+addButton.addEventListener('click', () => {
+    function calculate (myNumber){
+        return myNumber+1;
+}   localStorage.setItem (new Date, calculate('click'));
+})
